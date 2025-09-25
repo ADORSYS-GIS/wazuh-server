eb929e4897d66d1cacad35d48378cf6d314f662f
Armand Meppa <60611368+ArmandMeppa@users.noreply.github.com>
2025-09-25 14:12:41 +0100

    fix(install.sh): fix get_installed_version function on macos machines

--- scripts/install.sh
+++ scripts/install.sh
@@ -294,9 +294,9 @@ get_installed_version() {
         Darwin*)
             # macOS (PKG)
             if [ -f "/var/db/receipts/com.wazuh.pkg.wazuh-agent.plist" ]; then
-                version=$(plutil -p "/var/db/receipts/com.wazuh.pkg.wazuh-agent.plist" 2>/dev/null \
-                          | awk -F'"' '/PackageFileName/ {print $4}' \
-                          | sed -E 's/.*wazuh-agent-([0-9.]+-[0-9]+).*/\1/' || :)
+                plutil -p "/var/db/receipts/com.wazuh.pkg.wazuh-agent.plist" 2>/dev/null | \
+                  awk -F'"' '/PackageFileName/ {print $4}' | \
+                  sed -E 's/.*wazuh-agent-([0-9.]+-[0-9]+).*/\1/'
             else
                 warn_message "Cannot determine installed version on macOS."
                 return 1
